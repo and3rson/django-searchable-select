ee1ed47ab2f1156f94a4bb6358354f1949149566
and3rson <anderson.dunai@gmail.com>
2015-12-18 07:38:54 +0200

    Changed stuff.

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import os
 from setuptools import setup
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-readme = f.read()
-f.close()
-
 setup(
     name='django-searchable-select',
-    version='0.1',
-    description='django-searchable-select - a better and faster multiple selection widget with suggestions',
-    long_description=readme,
+    version='0.3',
+    description='django-searchable-select - a better and faster multiple selection widget with suggestions for Django',
+    long_description='A better and faster multiple selection widget with suggestions for Django. Provides replacement for standard widget of ManyToManyField.',
     author="Andrew Dunai",
     author_email='andrew@dun.ai',
     url='https://github.com/and3rson/django-searchable-select',
